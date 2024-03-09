1398d8f6d80fcd616064f06aefcdba23ceda0ae1
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-09 22:47:45 +0000

    Index component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,27 +1,222 @@
-import { StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        {/* PROFILE SECTION */}
+        <View style={[styles.rowContainer, {justifyContent: 'space-between'}]}>
+          <FontAwesome name="user-circle-o" size={25} color="grey" />
+          <View style={styles.rowContainer}>
+            <FontAwesome name="gift" size={25} color="grey" />
+            <Text>Get 10.0 GBP</Text>
+            <View>
+              <FontAwesome name="bell-o" size={25} color="black" />
+            </View>
+          </View>
+        </View>
+
+        {/* ACCOUNTS SECTION */}
+        <View style={[styles.rowContainer, {justifyContent: 'space-between'}]}>
+          <Text>My Accounts</Text>
+          <View style={styles.rowContainer}>
+            <Text>Hide balance
+              <FontAwesome name="eye-slash" color="black"/>
+            </Text>
+          </View>
+        </View>
+        {/* BALANCE SECTION */}
+        <View style={[styles.rowContainer, {justifyContent: 'space-between'}]}>
+          <View>
+            <Text>Flag</Text>
+            <Text>NGN</Text>
+            <Text>N 0.00 </Text>
+          </View>
+          <View>
+            <FontAwesome name="plus" size={25} color="black" />
+            <Text>Tap to add account</Text>
+          </View>
+        </View>
+        {/* ACTION BUTTONS */}
+        <View style={[styles.rowContainer, {justifyContent: 'space-between'}]}>
+          <View>
+            <Text>Send money</Text>
+          </View>
+          <View>
+            <Text>Add money</Text>
+          </View>
+            <FontAwesome name='ellipsis-h' size={25}/>
+        </View>
+      </View>
+      <View style={styles.secondHalf}>
+        {/* CTA */}
+        <View style={[styles.rowContainer, {justifyContent: 'space-between'}]}>
+          <Text>Get started</Text>
+          <Text>View all steps</Text>
+        </View>
+        {/* INVITE SECTION */}
+        <View>
+          <View style={[styles.rowContainer, { justifyContent: 'space-between' }]}>
+            <View>
+              <Text>Invite a friend and get £10.00</Text>
+              <Text style={{ width: 300}}>
+                They sign up and verify their identity.
+                They send up to £100.00 and you both get £10.00
+              </Text>
+            </View>
+            <View>
+              <FontAwesome name="user-circle-o" size={25} color="black" />
+            </View>
+          </View>
+          <View style={[styles.rowContainer, { justifyContent: 'space-between' }]}>
+            <View>
+              <Text>Invite Friends</Text>
+            </View>
+            <View>
+              <Text>Dismiss</Text>
+            </View>
+          </View>
+        </View>
+        {/* RECIPIENT LIST SECTION */}
+        <View style={styles.marginTop}>
+          <Text>Send To</Text>
+          <View style={[styles.rowContainer, {justifyContent: 'space-between'}]}>
+            <View>
+              <View style={styles.rounded}>
+                <Text>COO</Text>
+                <View style={[styles.rounded, styles.flagStyle]}><Text>Flag</Text></View>
+              </View>
+              <Text>Caleb O</Text>
+              <Text>kuda</Text>
+            </View>
+            <View>
+              <View></View>
+              <Text>Caleb O</Text>
+              <Text>kuda</Text>
+            </View>
+            <View>
+              <View></View>
+              <Text>Caleb O</Text>
+              <Text>kuda</Text>
+            </View>
+          </View>
+        </View>
+        {/* SERVICES SECTION */}
+        <View style={styles.marginTop}>
+          <Text>Services</Text>
+          <View style={[styles.rowContainer, {justifyContent: 'space-between'}]}>
+            <View>
+              <View></View>
+              <Text>Caleb O</Text>
+              <Text>kuda</Text>
+            </View>
+            <View>
+              <View></View>
+              <Text>Caleb O</Text>
+              <Text>kuda</Text>
+            </View>
+            <View>
+              <View></View>
+              <Text>Caleb O</Text>
+              <Text>kuda</Text>
+            </View>
+          </View>
+        </View>
+        {/* ACCOUNT INFORMATION SECTION */}
+        <View style={styles.marginTop}>
+          <Text>Account Information</Text>
+          <View style={[styles.rowContainer, {justifyContent: 'space-between', overflow: 'scroll', columnGap: 40}]}>
+            <View>
+              <View>
+                <Text>COO</Text>
+              </View>
+              <Text>Caleb O</Text>
+              <Text>kuda</Text>
+            </View>
+            <View>
+              <View></View>
+              <Text>Caleb O</Text>
+              <Text>kuda</Text>
+            </View>
+            <View>
+              <View></View>
+              <Text>Caleb O</Text>
+              <Text>kuda</Text>
+            </View>
+            <View>
+              <View>
+                <Text>COO</Text>
+              </View>
+              <Text>Caleb O</Text>
+              <Text>kuda</Text>
+            </View>
+            <View>
+              <View></View>
+              <Text>Caleb O</Text>
+              <Text>kuda</Text>
+            </View>
+            <View>
+              <View></View>
+              <Text>Caleb O</Text>
+              <Text>kuda</Text>
+            </View>
+            <View>
+              <View>
+                <Text>COO</Text>
+              </View>
+              <Text>Caleb O</Text>
+              <Text>kuda</Text>
+            </View>
+            <View>
+              <View></View>
+              <Text>Caleb O</Text>
+              <Text>kuda</Text>
+            </View>
+            <View>
+              <View></View>
+              <Text>Caleb O</Text>
+              <Text>kuda</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  secondHalf: {
+    backgroundColor: 'rgb(245 244 242)',
+    padding: 20,
+  },
+  marginTop: {
+    marginTop: 20,
+  },
+  rounded: {
+    borderRadius: 50,
+    backgroundColor: 'grey',
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  flagStyle: {
+    width: 20,
+    height: 20,
+    backgroundColor: 'green',
+    left: 20,
+    top: 10
+  },
+  rowContainer: {
+    flexDirection: 'row',
+  },
+  textWidth: {
+    width: 10,
   },
   separator: {
     marginVertical: 30,
