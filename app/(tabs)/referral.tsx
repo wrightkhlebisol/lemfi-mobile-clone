b33e8275bc319cb93a86b66be8346424136454c7
wrightkhlebisol <calebogundiya@gmail.com>
2024-03-15 01:37:18 +0000

    Style referral page

--- app/(tabs)/referral.tsx
+++ app/(tabs)/referral.tsx
@@ -10,46 +10,52 @@ export default function Referral() {
       // darkColor='rgb(85 89 148)'
     >
       <View>
-        <Text>Invite friends</Text>
+        <Text style={[styles.textColor, {fontWeight: '700', fontSize: 25}]}>Invite friends</Text>
       </View>
       <View>
-        <Text>Image Here</Text>
+        <Text style={styles.textColor}>Image Here</Text>
       </View>
-      <View>
-        <Text>Invite friends and earn £10.0</Text>
-        <Text>
+      <View style={{alignItems: 'center', gap: 20}}>
+        <Text style={[styles.textColor, {fontWeight: '600', fontSize: 25}]}>Invite friends and earn £10.0</Text>
+        <Text style={[styles.textColor, {textAlign: 'center', fontSize: 17}]}>
           Invite a friend and you both get GBP 10.0 each when they send over GBP 100.0 to supported countries
         </Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            backgroundColor: 'rgb(65 66 96)',
+            padding: 20,
+            borderRadius: 10,
+            width: '100%',
+            alignItems: 'center',
+            margin: 10,
+          }}
+          // lightColor='rgb(9 164 94)'
+          // darkColor='rgb(65 66 96)'
+        >
+          <View style={{gap: 4}}>
+            <Text style={[styles.textColor, {fontSize: 17}]}>Your referral code</Text>
+            <Text style={[styles.textColor, {fontSize: 25}]}>CALEKXXS</Text>
+          </View>
+          <View>
+            <Pressable style={styles.pressable}>
+              <Text style={styles.textColor}>Copy</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View
+          // lightColor='rgb(9 164 94)'
+          style={{width: '100%', gap: 5, alignItems: 'center'}}
+        >
+          <Pressable style={{justifyContent: 'space-around', backgroundColor: 'rgb(9 164 94)', padding: 25, borderRadius: 20, width: '100%', }}>
+            <Text style={[styles.textColor, {fontSize: 20, fontWeight: '600'}]}>Invite friends <FontAwesome name='chevron-right' /></Text>
+          </Pressable>
+          <Text style={styles.textColor}>*Terms and conditions apply</Text>
+        </View>
       </View>
 {/*   */}
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          backgroundColor: 'rgb(65 66 96)',
-        }}
-        // lightColor='rgb(9 164 94)'
-        // darkColor='rgb(65 66 96)'
-      >
-        <View>
-          <Text>Your referral code</Text>
-          <Text>CALEKXXS</Text>
-        </View>
-        <View>
-          <Pressable style={styles.pressable}>
-            <Text>Copy</Text>
-          </Pressable>
-        </View>
-      </View>
 
-      <View
-      // lightColor='rgb(9 164 94)'
-      >
-        <Pressable>
-          <Text>Invite friends <FontAwesome name='chevron-right' /></Text>
-        </Pressable>
-        <Text>*Terms and conditions apply</Text>
-      </View>
     </View>
   );
 }
@@ -58,10 +64,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    backgroundColor: 'rgb(85 89 148)',
+    padding: 30,
+    gap: 100,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  textColor: {
+    color: 'white',
   },
   separator: {
     marginVertical: 30,
